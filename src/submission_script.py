6cb9cd7ee68c5ac4ed0dccb89ad550fdac528134
pparaguya <philipp.paraguya@infomotion.de>
2019-02-04 23:47:41 -0800

    changed a lot; doesn't work

--- src/submission_script.py
+++ src/submission_script.py
@@ -1,20 +1,17 @@
 __author__ = "Robin Brecht, Philipp Paraguya"
 __credits__ = ["Robin Brecht", "Philipp Paraguya"]
 
-import os
 import json
 import pandas as pd
 import sys
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import cohen_kappa_score,  make_scorer
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
+from sklearn.base import clone
 
 from lightgbm import LGBMClassifier
 
@@ -26,6 +23,7 @@ def squared_cohen_kappa(y1, y2):
     TODO Kommentieren
     """
     return cohen_kappa_score(y1,y2)**2
+
 
 def import_data(pDATA_PATH):
     """
@@ -121,18 +119,9 @@ def import_data(pDATA_PATH):
 
     return df
 
-def main(argv):
-    """
-    Diese Funktion ist der Einstiegspunkt für dieses Projekt
-    """
 
-    DATA_PATH = argv[0]
-
-    df = import_data(DATA_PATH)
-    df.head()
-
+def feat_eng(df):
     descriptions = df.Description.fillna("no_desc").values
-
     vectorizer = TfidfVectorizer(strip_accents='unicode', analyzer='word', token_pattern=r'(?u)\b\w+\b', use_idf=True)
     X = vectorizer.fit_transform(list(descriptions))
 
@@ -148,6 +137,60 @@ def main(argv):
     X = pd.DataFrame(X, columns=['svd_{}'.format(i) for i in range(300)])
     df = pd.concat((df, X), axis=1)
 
+    return df
+
+
+def do_grid_search(lgbm, X, y):
+    grid = {'learning_rate': [0.1, 0.001, 0.003, 0.0005], 'max_bin': [100, 255, 400, 500],
+            'num_iterations': [50, 100, 150, 200, 300, 500]}
+
+    kappa_scorer = make_scorer(squared_cohen_kappa)
+    lgbm_cv = GridSearchCV(lgbm, grid, scoring=kappa_scorer, cv=3, verbose=10)
+    lgbm_cv.fit(X, y)
+
+    print("tuned hyperparameters :(best parameters) ", lgbm_cv.best_params_)
+    print("accuracy :", lgbm_cv.best_score_)
+
+    return lgbm_cv
+
+
+def train_and_run_cv(model, X, y, cv=3):
+    skf = StratifiedKFold(n_splits=3)
+    i = 0
+    cv_score = []
+    pred_test_full = 0
+
+    for train_index, test_index in skf.split(X, y):
+        i += 1
+        print("training fold {} of {}".format(i, cv))
+        X_train, X_test = np.array(X)[train_index, :], np.array(X)[test_index, :]
+        y_train, y_test = np.array(y)[train_index], np.array(y)[test_index]
+
+        model_copy = clone(model)
+        model_copy.fit(X_train, y_train)
+        score = squared_cohen_kappa(y_test, model_copy.predict(X_test))
+        print("Score:", score)
+        cv_score.append(score)
+        pred_test = model_copy.predict(X_test)
+        pred_test_full += pred_test
+
+    print("Mean cv Score", np.mean(cv_score))
+
+    y_pred = pred_test_full / cv
+
+    return model_copy, pred_test_full
+
+
+def main(argv):
+    """
+    Diese Funktion ist der Einstiegspunkt für dieses Projekt
+    """
+
+    DATA_PATH = argv[0]
+
+    df = import_data(DATA_PATH)
+    df = feat_eng(df)
+
     df_train, df_test = train_test_split(df, test_size=0.2)
 
     drop_feat_list = ['AdoptionSpeed', 'Name', 'RescuerID', 'Description', 'PetID', 'label_description']
@@ -158,21 +201,16 @@ def main(argv):
     X = df_train[feature_list]
     y = df_train['AdoptionSpeed'].values
 
-    grid = {'learning_rate': [0.1, 0.001, 0.003, 0.0005], 'max_bin': [100, 255, 400, 500],
-            'num_iterations': [50, 100, 150, 200, 300, 500]}
     lgbm = LGBMClassifier(objective='multiclass', random_state=5)
 
-    kappa_scorer = make_scorer(squared_cohen_kappa)
-    lgbm_cv = GridSearchCV(lgbm,grid, scoring=kappa_scorer,cv=3)
-    lgbm_cv.fit(X, y)
+    lgbm, pred  = train_and_run_cv(lgbm, X, y)
 
-    print("tuned hpyerparameters :(best parameters) ", lgbm_cv.best_params_)
-    print("accuracy :", lgbm_cv.best_score_)
+    #lgbm = do_grid_search(X, y)
 
-    df_test['lgbm_pred'] = lgbm_cv.predict(df_test[feature_list])
+    #df_test['lgbm_pred'] = lgbm.predict(df_test[feature_list])
+    df_test['lgbm_pred'] = pred
 
     lgbm_kappa = squared_cohen_kappa(df_test['AdoptionSpeed'], df_test['lgbm_pred'])
-
     print('Model tested! Squared Cohen Kappa: ' + str(lgbm_kappa))
 
     df_test[['PetID', 'lgbm_pred']].to_csv('submission.csv', index=False, header=['PetID', 'AdoptionSpeed'])
@@ -180,6 +218,9 @@ def main(argv):
     feature_imp = pd.DataFrame(sorted(zip(lgbm.feature_importances_, X.columns)), columns=['Value', 'Feature'])
 
     print(feature_imp)
+
+
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
