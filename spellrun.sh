7b974fd660fd158483c8cac20904ad9ae875f7e3
Philipp Paraguya <philippparaguya@gmail.com>
2019-03-06 14:03:46 +0100

    changed mount parameter for simlink

--- spellrun.sh
+++ spellrun.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-spell run -m uploads/petfinder:petfinder -t cpu --pip-req requirements.txt "python src/submission_script.py ../petfinder/"
+spell run -m petfinder:petfinder -t cpu --pip-req requirements.txt "python src/submission_script.py ../petfinder/"
