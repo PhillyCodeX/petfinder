e924bc2bf327d64c733c72507e65b22c214edf00
Philipp Paraguya <philippparaguya@gmail.com>
2019-03-06 09:32:28 +0100

    changed spellrun to new dataset

--- spellrun.sh
+++ spellrun.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-spell run -m uploads/petfinder_train:petfinder_train -t K80x8 --pip-req configs/requirements.txt "python main.py"
+spell run -m uploads/petfinder:petfinder -t cpu --pip-req configs/requirements.txt "python src/submission_script.py"
