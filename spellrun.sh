d4b67e1ad9de90cc935f2b38015d3bd22b6af167
Philipp Paraguya <philippparaguya@gmail.com>
2019-03-06 15:56:21 +0100

    removed Spell from gitignore and changed spell machine to cpu-huge

--- spellrun.sh
+++ spellrun.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-spell run -m petfinder:petfinder -t cpu-big --pip-req requirements.txt "python src/submission_script.py ./petfinder"
+spell run -m petfinder:petfinder -t cpu-huge --pip-req requirements.txt "python src/submission_script.py ./petfinder"
