7ccf0cdf7ea4ecd951616a5f00e802e3029902af
Philipp Paraguya <philippparaguya@gmail.com>
2019-03-11 17:49:50 +0100

    changed machine to ram-huge machine (incl. more CPU)

--- spellrun.sh
+++ spellrun.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-spell run -m petfinder:petfinder -t cpu-huge --pip-req requirements.txt "python src/submission_script.py ./petfinder"
+spell run -m petfinder:petfinder -t ram-huge --pip-req requirements.txt "python src/submission_script.py ./petfinder"
